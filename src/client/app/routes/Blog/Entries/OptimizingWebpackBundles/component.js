4ff0391a7200127c5609aed51dfb387e89c8ab5a
Jaspreet Singh <jaspreet@joinhoney.com>
2018-04-02 21:13:25 -0700

    fix(blog): Polish up the writing for webpack bundle blog post

--- src/client/app/routes/Blog/Entries/OptimizingWebpackBundles/component.js
+++ src/client/app/routes/Blog/Entries/OptimizingWebpackBundles/component.js
@@ -15,7 +15,7 @@ class BlogEntry extends PureComponent {
           <H1>Optimizing Webpack Bundles for Production React Apps</H1>
           <P>
             In building this site, I wanted to practice techniques for limiting bloat and unnecessary network requests
-            that inevitably creep into most front end projects that don't put perf first.
+            that inevitably creep into most front end projects that don't put performance first.
             Here are three techniques outside of the obvious basic gzip + CDN Caching that you may find helpful in
             making your site more efficient.
           </P>
@@ -27,23 +27,24 @@ class BlogEntry extends PureComponent {
             href="https://github.com/jamiebuilds/react-loadable#------------guide">guide</Link> on the
             <CodeSnippet>react-loadable</CodeSnippet> github page, but the basic idea is that you can split up your
             website into different
-            bundles based on routes. This is really important for homepages for example where you may have just a simple
+            bundles based on routes. This is especially important for homepages for example where you may have just a
+            simple
             splash page, but your site is downloading tons of unnecessary javascript that may not be needed at all if
-            for example a user doesn't login. Image from the github showing this difference in bundling:
+            for example a user doesn't login. Considering the short attention span of most users, you may lose out on
+            users sticking around entirely.
+            Image from the github repo showing this difference in bundling:
           </P>
 
           <Image
             src="https://camo.githubusercontent.com/129f34bba8ba80f8a4a1a037619b5f7076906e06/687474703a2f2f7468656a616d65736b796c652e636f6d2f696d672f72656163742d6c6f616461626c652d73706c69742d62756e646c65732e706e67"/>
           <P>
             Beyond route level code splitting, sometimes it also makes sense to split out some data specific bundle
-            that you'd like
-            to live separately from your visual library code, for caching purposes for example. Whenever I deploy a
-            new blog post for example,
-            it doesn't make sense to bust the cache for the entire <CodeSnippet>/blog</CodeSnippet> route, wouldn't it
-            make more sense to just
-            bust a data specific cache that contains a list of all possible blog entries?
-            That's exactly what I do, here's the code below that takes advantage of the dynamic import syntax to
-            import a DAO for my blog posts:
+            that you'd like to live separately from the rest of your bundles. Whenever I deploy a
+            new blog post for example, it doesn't make sense to bust the cache for the
+            entire <CodeSnippet>/blog</CodeSnippet> route, wouldn't it
+            make more sense to only cache bust a data specific "db" bundle that contains a list of all possible blog entries?
+            Here's a code example below from this site that takes advantage of dynamic import syntax to
+            implement this data-specific chunk feature for my blog posts:
           </P>
           <Code code={`
 async componentDidMount() {
@@ -55,21 +56,23 @@ async componentDidMount() {
             Without understanding the weight of your production dependencies,
             you're bound to ship megabytes over the wire for some functionality
             that could fit in kilobytes instead. You should ideally include some sort of automated process for every PR
-            that if there is a package.json change, show what the effects are on the production build bundles.
+            such that if there is a package.json change, show what the effects are on the production build bundles
+            before actually merging.
           </P>
           <P>
             One easy way of doing this is through <Link href="https://www.npmjs.com/package/webpack-bundle-analyzer">Webpack
             Bundle Analyzer</Link>
           </P>
           <P>
-            Real world use case: I needed a way to create URL safe routes for this site for my blog posts based on their
-            titles, and typically the way I've seen
-            this done is through some slug process, so I first decided to use
+            Real world use case: I needed a way to create URL safe routes on this site for my blog posts based on their
+            titles. Typically the way I've seen
+            this done is through a slug process, so I first decided to use
             the <CodeSnippet>slug</CodeSnippet> package for this feature. When
             I went to build my site however, I was shocked at how much extra weight was
             tacked on by what I assumed was a simple library. I ended up using Webpack Bundle Analyzer to first detect
             this issue, and then swapped it out
-            for a slimmer library <CodeSnippet>slugify</CodeSnippet> that fit my simple purposes. Visuals from Webpack
+            for a slimmer <CodeSnippet>slugify</CodeSnippet> library that worked for my simple purpose. Visuals from
+            Webpack
             Bundle Analyzer below:
           </P>
 
@@ -91,9 +94,8 @@ async componentDidMount() {
             While many libraries are adopting the
             new <CodeSnippet>sideEffects</CodeSnippet> <Link
             href="https://github.com/webpack/webpack/tree/master/examples/side-effects">webpack 4
-            standard</Link> to save you the trouble, you can bullet proof yourself by importing functionality from a
-            library from where the code
-            lives directly rather than from a top level index file.
+            standard</Link> to save you the trouble, you can bullet proof yourself by importing functionality from where
+            library code lives directly rather than from a top level index file import.
           </P>
           <P>
             Real world use case: This site uses a couple icons from the <CodeSnippet>react-icons</CodeSnippet> library.
